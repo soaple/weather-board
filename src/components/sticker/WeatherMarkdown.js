8f68df8d5094e86e94bf02beceb459bee572668b
Soaple <inje@soaple.io>
2020-09-13 20:29:12 +0900

    Sample data updated.

--- src/components/sticker/WeatherMarkdown.js
+++ src/components/sticker/WeatherMarkdown.js
@@ -3,7 +3,7 @@ import { Markdown } from '@stickyboard/markdown';
 
 function WeatherMarkdown(props) {
     const [content, setContent] = useState(
-        "# Today's weather\n## Sunny\n### Forecast"
+        "# Today's weather\n## Today's weather is expected to be clear, with temperatures expected to be as low as 25 degrees Celsius and as high as 30 degrees Celsius.\n## It is expected to rain all day tomorrow, so please take an umbrella with you when you go out."
     );
 
     return (
